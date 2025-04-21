a5ef29960ad78f3231ca60cf76d8f37f1c0ec627
korges <korgesik@gmail.com>
2025-04-21 11:55:07 +0200

    Query to fetch dashboard

--- frontend/src/app/governance/page.tsx
+++ frontend/src/app/governance/page.tsx
@@ -2,6 +2,7 @@
 
 import { useNetworkVariable } from "@/config/networkConfig";
 import { TESTNET_DASHBOARD_ID } from "@/constants";
+import { useSuiClientQuery } from "@mysten/dapp-kit";
 
 const PROPOSAL_COUNT = 7;
 
@@ -19,18 +20,30 @@ const ProposalItem = () => {
   const ProposalView = () => {
     const dashboardId = useNetworkVariable("dashboardId");
 
-    console.log(dashboardId)
+    const { data: dataResponse, isPending, error } = useSuiClientQuery(
+      "getObject", {
+        id: dashboardId,
+        options: {
+          showContent: true
+        }
+      }
+    );
 
-    
+    if (isPending) return <div className="text-center text-gray-500">Loading...</div>;
+    if (error) return <div className="text-red-500">Error: { error.message }</div>;
+    if (!dataResponse.data) return <div className="text-center text-gray-500">Not found...</div>;
+
+    debugger
 
     return (
       <>
         <h1 className="text-4xl font-bold mb-8">New Proposals</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {
+          {/* {
             new Array(PROPOSAL_COUNT).fill(1).map((id) =>
               <ProposalItem key={id * Math.random()}/>
-          )}
+          )} */}
+          {JSON.stringify(dataResponse)}
         </div>
       </>
     )
