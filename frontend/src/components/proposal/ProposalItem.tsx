3e331f2f269a3ac203e8d8436e3d674d05f55485
korges <korgesik@gmail.com>
2025-04-21 21:00:03 +0200

    Display vote vount & expiration time

--- frontend/src/components/proposal/ProposalItem.tsx
+++ frontend/src/components/proposal/ProposalItem.tsx
@@ -29,12 +29,25 @@ export const ProposalItem: FC<ProposalItemProps> = ({ id }) => {
   if(!proposal) return <CustomText text="No data found"/>
 
   return (
-    <div>
       <div className="p-4 border rounded-lg shadow-sm wg-white dark:bg-gray-800 hover:border-blue-500">
         <p className="text-xl font-semibold mb-2">Title: {proposal.title}</p>
         <p className="text-gray-700 dark:text-gray-300">{proposal.description}</p>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex space-x-4">
+            <div className="flex items-center text-green-600">
+              <span className="mr-1">👍</span>
+              {proposal.votedYesCount}
+            </div>
+            <div className="flex items-center text-red-600">
+              <span className="mr-1">👎</span>
+              {proposal.votedNoCount}
+            </div>
+          </div>
+          <div>
+            <CustomText text={formatUnixTime(proposal.expiration)}/>
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
@@ -49,4 +62,15 @@ function parseProposal(data: SuiObjectData): Proposal | null {
     votedNoCount: Number(voted_yes_count),
     expiration: Number(expiration)
   }
+}
+
+function formatUnixTime(timestampSec: number) {
+  return new Date(timestampSec * 1000).toLocaleDateString("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  });
 }
