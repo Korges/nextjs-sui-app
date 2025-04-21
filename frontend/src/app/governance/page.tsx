a0192bca31bac42cb7f1fa1c5da8b049a1be63bf
korges <korgesik@gmail.com>
2025-04-21 12:18:14 +0200

    Extract proposal ids

--- frontend/src/app/governance/page.tsx
+++ frontend/src/app/governance/page.tsx
@@ -3,14 +3,18 @@
 import { useNetworkVariable } from "@/config/networkConfig";
 import { TESTNET_DASHBOARD_ID } from "@/constants";
 import { useSuiClientQuery } from "@mysten/dapp-kit";
+import { SuiObjectData } from "@mysten/sui/client";
+import { FC } from "react";
 
-const PROPOSAL_COUNT = 7;
+type ProposalItemProps = {
+  id: string
+};
 
-const ProposalItem = () => {
+const ProposalItem: FC<ProposalItemProps> = ({id}) => {
     return (
       <div>
         <div className="p-4 border rounded-lg shadow-sm wg-white dark:bg-gray-800 hover:border-blue-500">
-          <p className="text-xl font-semibold mb-2">Title: Hello There</p>
+          <p className="text-xl font-semibold mb-2">Title: {id}</p>
           <p className="text-gray-700 dark:text-gray-300">Desc: What is your vote?</p>
         </div>
       </div>
@@ -33,20 +37,29 @@ const ProposalItem = () => {
     if (error) return <div className="text-red-500">Error: { error.message }</div>;
     if (!dataResponse.data) return <div className="text-center text-gray-500">Not found...</div>;
 
-    debugger
-
     return (
       <>
         <h1 className="text-4xl font-bold mb-8">New Proposals</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* {
-            new Array(PROPOSAL_COUNT).fill(1).map((id) =>
-              <ProposalItem key={id * Math.random()}/>
-          )} */}
-          {JSON.stringify(dataResponse)}
+          {getDashboardFields(dataResponse.data)?.proposals_ids.map(id => 
+            <ProposalItem key={id} id={id}/>
+          )}
         </div>
       </>
     )
+  }
+
+  type SuiID = {
+    id: string
+  };
+
+  function getDashboardFields(data: SuiObjectData) {
+    if (data.content?.dataType !== "moveObject") return null;
+    
+    return data.content.fields as {
+      id: SuiID,
+      proposals_ids: string[]
+    };
   }
 
   export default ProposalView;
